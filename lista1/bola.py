b1eb361e731411a7e32b74a818ad498f545e3269
GuilhermeLBonomo <Darkish5044@proton.me>
2023-08-26 18:59:55 -0300

    Questao 1

---
+++ lista1/bola.py
@@ -0,0 +1,53 @@
+from sys import exception
+
+
+class Bola:
+    def __init__(self, color, circunference: float, material: str) -> None:
+        self.__color = color.capitalize()
+        self.__circunference = circunference
+        self.__material = material.capitalize()
+
+    def __str__(self) -> str:
+        return f"Color: {self.__color}\nCircunference: {self.__circunference}\nMaterial: {self.__material}"
+
+    def trocaCor(self, new_color) -> None:
+        if isinstance(new_color, str):
+            self.__color == new_color.capitalize()
+        elif (isinstance(new_color, list) or isinstance(new_color, tuple)) and len(
+            new_color
+        ) == 3:
+            for num in new_color:
+                num = int(num)
+                if 0 <= num <= 255:
+                    self.__color = new_color
+        else:
+            raise ValueError
+
+    def mostaCor(self) -> None:
+        print(self.__color)
+
+    @property
+    def circunference(self) -> float:
+        return self.__circunference
+
+    @circunference.setter
+    def circunference(self, value) -> None:
+        if isinstance(value, str):
+            value = value.replace(",", ".")
+        try:
+            self.__circunference = float(value)
+        except:
+            raise ValueError
+
+    @property
+    def material(self) -> str:
+        return self.__material
+
+    @material.setter
+    def material(self, m) -> None:
+        self.__material = m
+
+
+if __name__ == "__main__":
+    b = Bola("Black", 2.781, "ferro")
+    print(b)
